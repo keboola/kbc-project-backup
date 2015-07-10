e90204c340614c706b0278a6fb8fcafb2d7e0084
Martin Halamíček <martin@halamicek.cz>
2015-07-10 11:08:17 +0200

    init

---
+++ README.md
@@ -0,0 +1,47 @@
+# Keboola Connection Project Backup
+
+Keboola Connection project backup to AWS S3 implemented as docker bundle.
+
+
+## Install & build
+
+```
+git clone https://github.com/keboola/kbc-project-backup.git
+cd kbc-project-backup
+curl https://s3.amazonaws.com/keboola-storage-api-cli/builds/sapi-client.0.2.4.phar > ./sapi-client.phar
+composer install
+```
+
+
+
+
+## Run
+```
+php ./src/run.php --data=./tests/data
+```
+
+Where `/data` goes to your data folder.
+
+
+## Data directory
+
+Data directory must follow conventions defined in [Keboola Docker Bundle repository](https://github.com/keboola/docker-bundle).
+
+
+## Configuration
+
+
+- YAML configuration stored in `data/config.yml`
+- KBC_TOKEN environment variable is required
+
+### Sample configuration
+Mapped to `/data/config.yml`
+
+```
+parameters:
+  awsAccessKeyId: asdf
+  awsSecretAccessKey: sadf
+  s3bucket: test
+  s3path: /
+
+```
---
+++ src/run.php
@@ -0,0 +1,50 @@
+<?php
+
+use Symfony\Component\Yaml\Yaml;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+require_once(dirname(__FILE__) . "/../vendor/autoload.php");
+
+$arguments = getopt("d::", ["data::"]);
+if (!isset($arguments["data"])) {
+	print "Data folder not set.";
+	exit(1);
+}
+
+$token = getenv('KBC_TOKEN');
+if (!$token) {
+	print "KBC_TOKEN must be set";
+	exit(1);
+}
+
+$resolver = new OptionsResolver();
+$resolver->setDefaults([
+	's3path' => '/'
+]);
+$resolver
+	->setRequired('s3bucket')
+	->setRequired('awsAccessKeyId')
+	->setRequired('awsSecretAccessKey');
+
+
+$config = Yaml::parse(file_get_contents($arguments["data"] . "/config.yml"));
+
+try {
+	$parameters = $resolver->resolve($config['parameters']);
+} catch (\Exception $e) {
+	print $e->getMessage();
+	exit(1);
+}
+
+putenv("AWS_ACCESS_KEY_ID={$parameters['awsAccessKeyId']}");
+putenv("AWS_SECRET_ACCESS_KEY={$parameters['awsSecretAccessKey']}");
+
+$return = null;
+passthru('php ' . __DIR__ . '/../sapi-client.phar --token=' .
+	escapeshellarg($token) .
+	' backup-project ' .
+	escapeshellarg($parameters['s3bucket']) .
+	escapeshellarg($parameters['s3path'])
+, $return);
+
+exit($return);
