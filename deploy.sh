645ab77d26b414539dbf30c5b250e63441b653db
Ondrej Hlavacek <ondrej.hlavacek@keboola.com>
2017-09-19 14:49:59 +0200

    feat(deploy): update developer portal

--- deploy.sh
+++ deploy.sh
@@ -12,3 +12,7 @@ eval $(docker run --rm \
     quay.io/keboola/developer-portal-cli-v2:latest ecr:get-login keboola kbc-project-backup)
 docker push ${REPOSITORY}:${TRAVIS_TAG}
 docker push ${REPOSITORY}:latest
+
+docker run --rm -e KBC_DEVELOPERPORTAL_USERNAME -e KBC_DEVELOPERPORTAL_PASSWORD -e KBC_DEVELOPERPORTAL_URL \
+    quay.io/keboola/developer-portal-cli-v2:latest update-app-repository keboola kbc-project-backup ${TRAVIS_TAG}
+
