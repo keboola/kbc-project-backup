5a20e5909f454b41b687333608e6e9ded4c50da3
Ondrej Hlavacek <ondrej.hlavacek@keboola.com>
2017-09-19 14:04:53 +0200

    fix(deploy): repo names

--- deploy.sh
+++ deploy.sh
@@ -3,12 +3,12 @@
 docker pull quay.io/keboola/developer-portal-cli-v2:latest
 export REPOSITORY=`docker run --rm  \
     -e KBC_DEVELOPERPORTAL_USERNAME -e KBC_DEVELOPERPORTAL_PASSWORD -e KBC_DEVELOPERPORTAL_URL \
-    quay.io/keboola/developer-portal-cli-v2:latest ecr:get-repository ${KBC_DEVELOPERPORTAL_VENDOR} ${KBC_DEVELOPERPORTAL_APP}`
-docker tag ${KBC_APP_REPOSITORY}:latest ${REPOSITORY}:${TRAVIS_TAG}
-docker tag ${KBC_APP_REPOSITORY}:latest ${REPOSITORY}:latest
+    quay.io/keboola/developer-portal-cli-v2:latest ecr:get-repository keboola kbc-project-backup`
+
+docker tag keboola/kbc-project-backup:latest ${REPOSITORY}:${TRAVIS_TAG}
+docker tag keboola/kbc-project-backup:latest ${REPOSITORY}:latest
 eval $(docker run --rm \
     -e KBC_DEVELOPERPORTAL_USERNAME -e KBC_DEVELOPERPORTAL_PASSWORD -e KBC_DEVELOPERPORTAL_URL \
-    quay.io/keboola/developer-portal-cli-v2:latest ecr:get-login ${KBC_DEVELOPERPORTAL_VENDOR} ${KBC_DEVELOPERPORTAL_APP})
+    quay.io/keboola/developer-portal-cli-v2:latest ecr:get-login keboola kbc-project-backup)
 docker push ${REPOSITORY}:${TRAVIS_TAG}
 docker push ${REPOSITORY}:latest
-
