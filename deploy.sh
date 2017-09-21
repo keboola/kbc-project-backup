6dd35bf7ebc941795a70bf417fe1d8cf8969f2f7
Ondrej Hlavacek <ondrej.hlavacek@keboola.com>
2017-09-21 14:09:30 +0200

    fix(deploy): correct app

--- deploy.sh
+++ deploy.sh
@@ -3,16 +3,15 @@
 docker pull quay.io/keboola/developer-portal-cli-v2:latest
 export REPOSITORY=`docker run --rm  \
     -e KBC_DEVELOPERPORTAL_USERNAME -e KBC_DEVELOPERPORTAL_PASSWORD -e KBC_DEVELOPERPORTAL_URL \
-    quay.io/keboola/developer-portal-cli-v2:latest ecr:get-repository keboola kbc-project-backup`
-
+    quay.io/keboola/developer-portal-cli-v2:latest ecr:get-repository keboola kbc-project-takeout`
 docker tag keboola/kbc-project-backup:latest ${REPOSITORY}:${TRAVIS_TAG}
 docker tag keboola/kbc-project-backup:latest ${REPOSITORY}:latest
 eval $(docker run --rm \
     -e KBC_DEVELOPERPORTAL_USERNAME -e KBC_DEVELOPERPORTAL_PASSWORD -e KBC_DEVELOPERPORTAL_URL \
-    quay.io/keboola/developer-portal-cli-v2:latest ecr:get-login keboola kbc-project-backup)
+    quay.io/keboola/developer-portal-cli-v2:latest ecr:get-login keboola kbc-project-takeout)
 docker push ${REPOSITORY}:${TRAVIS_TAG}
 docker push ${REPOSITORY}:latest
 
 docker run --rm -e KBC_DEVELOPERPORTAL_USERNAME -e KBC_DEVELOPERPORTAL_PASSWORD -e KBC_DEVELOPERPORTAL_URL \
-    quay.io/keboola/developer-portal-cli-v2:latest update-app-repository keboola kbc-project-backup ${TRAVIS_TAG}
+    quay.io/keboola/developer-portal-cli-v2:latest update-app-repository keboola kbc-project-takeout ${TRAVIS_TAG}
 
