d442b332d07ef5b20e968e17e7ea5cdee1ec0c78
Ondra Jodas <ondrej.jodas@keboola.com>
2021-06-28 12:16:48 +0200

    wip

--- src/Config/ConfigDefinition.php
+++ src/Config/ConfigDefinition.php
@@ -6,6 +6,7 @@ namespace Keboola\BackupProject\Config;
 
 use Keboola\Component\Config\BaseConfigDefinition;
 use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
+use Symfony\Component\Config\Definition\Exception\InvalidConfigurationException;
 
 class ConfigDefinition extends BaseConfigDefinition
 {
